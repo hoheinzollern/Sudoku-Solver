22ba497df646e6592a2663b2c723504f1c33b854
Alessandro Bruni <alessandro.bruni@gmail.com>
2010-06-03 20:26:30 +0200

    - Branch initialized - Directory structure

---
+++ src/SudokuSolverApplet.scala
@@ -0,0 +1,14 @@
+/**
+ * Sudoku Solver
+ * 
+ * Authors: Emanuele Fabbri, Alessandro Bruni
+ */
+
+import javax.swing.JApplet
+import java.awt._
+
+class SudokuSolverApplet extends JApplet {
+	override def init = {
+		Console.println("Hello World!")
+	}
+}
