c77eb4fe626a0668113dbc7347eca14302d7cf2c
alessandro <alessandro.bruni@gmail.com>
2010-07-07 23:18:55 +0200

    - Added UML diagrams

--- src/SudokuSolverApplet.scala
+++ src/SudokuSolverApplet.scala
@@ -4,11 +4,22 @@
  * Authors: Emanuele Fabbri, Alessandro Bruni
  */
 
-import javax.swing.JApplet
+import javax.swing._
 import java.awt._
+import java.awt.event._
 
 class SudokuSolverApplet extends JApplet {
+	var _layout = new GridLayout
+	var helloButton = new JButton("Hell-o world")
+
 	override def init = {
-		Console.println("Hello World!")
+		setLayout(_layout)
+		add(helloButton)
+		
+		helloButton.addActionListener(new ActionListener {
+			def actionPerformed(evt: ActionEvent) {
+				println("Hell-o world")
+			}
+		})
 	}
 }
