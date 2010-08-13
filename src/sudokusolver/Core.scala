6390150820e1483f514de07f7827fcc4dc0e9a12
Alessandro Bruni <alessandro.bruni@gmail.com>
2010-08-13 17:00:26 +0200

    - Sudoku generator started

--- src/sudokusolver/Core.scala
+++ src/sudokusolver/Core.scala
@@ -17,8 +17,28 @@ object Core {
 	 *  - 3 hard  
 	 */
 	def makeSudoku(level: Int): Sudoku = {
-		// FIXME: implement
-		throw new exceptions.NotImplementedException
+		var board = new Array[Array[Int]](9, 9)
+		// Initialize board to correct values
+		for (i <- 0 to 8) {
+			for (j <- 0 to 8) {
+				board(i)(j) = ((j + i * 3 + i / 3) % 9 + 1)
+			}
+		}
+		// Shuffle values between rows and columns
+		while (true) {
+			// FIXME: use correct methods
+			val i = (Math.random * 100).asInstanceOf[Int] % 3
+			val j = (Math.random * 100).asInstanceOf[Int] % 3
+			val k = (Math.random * 100).asInstanceOf[Int] % 3
+			if (i != j) {
+				val swap = board(i + j*3)
+				board(i + j*3) = board(i + k*3)
+				board(i + k*3) = swap
+			}
+		}
+		var sudoku = new Sudoku
+		sudoku.setBoard(board)
+		return sudoku
 	}
 	
 	/**
